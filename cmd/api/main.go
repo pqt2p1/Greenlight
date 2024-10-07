9f8831e17e494f5e1018ed60520d4f3717e30de8
Toan Pham <pqt2p1@gmail.com>
2024-10-07 12:24:42 +0700

    encapsulating routes in routes.go using httprouter package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -35,18 +35,16 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
 
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
-	logger.Printf("starting %s server on %s", cfg.env, cfg.port)
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 	err := srv.ListenAndServe()
 	logger.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pqt2p1/Greenlight
 
 go 1.23.1
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
