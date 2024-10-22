6b4353d5524812c074158f7451ee860d1fe7d331
Toan Pham <pqt2p1@gmail.com>
2024-10-22 12:25:06 +0700

    generating token

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -12,15 +12,16 @@ import (
 )
 
 const (
-	ScopeActivation = "activation"
+	ScopeActivation     = "activation"
+	ScopeAuthentication = "authentication"
 )
 
 type Token struct {
-	Plaintext string
-	Hash      []byte
-	UserID    int64
-	Expiry    time.Time
-	Scope     string
+	Plaintext string    `json:"token"`
+	Hash      []byte    `json:"-"`
+	UserID    int64     `json:"-"`
+	Expiry    time.Time `json:"expiry"`
+	Scope     string    `json:"-"`
 }
 
 func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
@@ -72,11 +73,11 @@ func (m TokenModel) Insert(token *Token) error {
 
 	args := []any{token.Hash, token.UserID, token.Expiry, token.Scope}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3 *time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	_, err := m.DB.ExecContext(ctx, query, args...)
-	return err 
+	return err
 }
 
 func (m TokenModel) DeleteAllForUser(scope string, userID int64) error {
@@ -85,11 +86,9 @@ func (m TokenModel) DeleteAllForUser(scope string, userID int64) error {
 		WHERE scope = $1 AND user_id = $2
 	`
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
-	defer cancel()  
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
 	_, err := m.DB.ExecContext(ctx, query, scope, userID)
 	return err
 }
-
-
