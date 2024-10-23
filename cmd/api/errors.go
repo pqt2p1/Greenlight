a14747d5590a9d72d81346adf51cc29aeb6c0aa7
Toan Pham <pqt2p1@gmail.com>
2024-10-23 09:41:56 +0700

    permission based authorize api

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -67,9 +67,17 @@ func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *htt
 
 func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("WWW-Authenticate", "Bearer")
-	
+
 	message := "invalid or missing authentication token"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
 
+func (app *application) authenticationRequiredResponse(w http.ResponseWriter, r *http.Request) {
+	message := "you must authenticated to access this resource"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
 
+func (app *application) inactiveAccountResponse(w http.ResponseWriter, r *http.Request) {
+	message := "your user account must be activated to access this resource"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
